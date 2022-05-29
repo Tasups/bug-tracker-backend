eb3a1fda2e40fd9e9f5fbecc0787fe555a686c39
Jason Whisnant <tasups@gmail.com>
2022-05-29 17:43:53 -0400

    added middleware and errors directories

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+
+
+
+// EXPRESS TOOLS
+app.use(express.json())
+
+// MIDDLEWARE
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+// ROUTES
+app.use('/api/v1/issues', issues)
+
+const port = process.env.PORT || 5000
+
+const start = () => {
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`)
+  })
+}
+
+start()
